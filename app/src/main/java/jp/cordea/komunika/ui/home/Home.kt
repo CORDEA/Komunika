c939886f494155e051250f231033e50e99a544fc
Yoshihiro Tanaka <contact@cordea.jp>
2021-09-11 13:17:56 +0900

    define list item

--- app/src/main/java/jp/cordea/komunika/ui/home/Home.kt
+++ app/src/main/java/jp/cordea/komunika/ui/home/Home.kt
@@ -1,6 +1,17 @@
 package jp.cordea.komunika.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import jp.cordea.komunika.ui.theme.KomunikaTheme
 
 @Composable
 fun Home() {
@@ -10,4 +21,43 @@ fun Home() {
 @Composable
 fun HomeContent() {
 
+}
+
+@Composable
+private fun HomeItem(viewModel: HomeItemViewModel) {
+    Card(Modifier.height(320.dp)) {
+        Box {
+            Image(
+                painter = rememberImagePainter(data = viewModel.thumbnailUrl),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .padding(16.dp)
+            ) {
+                Text(viewModel.title)
+                Spacer(Modifier.height(8.dp))
+                Text(viewModel.body)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewHomeItem() {
+    KomunikaTheme {
+        HomeItem(
+            HomeItemViewModel(
+                id = 0L,
+                title = "title",
+                body = "body",
+                thumbnailUrl = ""
+            )
+        )
+    }
 }
---
+++ app/src/main/java/jp/cordea/komunika/ui/home/HomeViewModel.kt
@@ -0,0 +1,11 @@
+package jp.cordea.komunika.ui.home
+
+class HomeViewModel {
+}
+
+class HomeItemViewModel(
+    val id: Long,
+    val thumbnailUrl: String,
+    val title: String,
+    val body: String
+)
