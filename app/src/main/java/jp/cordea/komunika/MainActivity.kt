85332a2e6c3f6b34dc23622190e5134fd24c2279
Yoshihiro Tanaka <contact@cordea.jp>
2021-09-12 12:39:10 +0900

    introduce dagger

--- app/src/main/java/jp/cordea/komunika/MainActivity.kt
+++ app/src/main/java/jp/cordea/komunika/MainActivity.kt
@@ -3,9 +3,11 @@ package jp.cordea.komunika
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import jp.cordea.komunika.ui.KomunikaApp
 import jp.cordea.komunika.ui.theme.KomunikaTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
