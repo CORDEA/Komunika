e5f2932af6ff8197d61944d7d04694267b851298
Yoshihiro Tanaka <contact@cordea.jp>
2021-09-12 14:28:42 +0900

    fetch items

--- app/src/main/java/jp/cordea/komunika/ui/home/HomeViewModel.kt
+++ app/src/main/java/jp/cordea/komunika/ui/home/HomeViewModel.kt
@@ -1,14 +1,43 @@
 package jp.cordea.komunika.ui.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.cordea.komunika.repository.ContactRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val repository: ContactRepository
+    repository: ContactRepository
 ) : ViewModel() {
+    init {
+        repository.findAll()
+            .map { list ->
+                list.map {
+                    HomeItemViewModel(
+                        it.id,
+                        "",
+                        "${it.firstName} ${it.lastName}",
+                        ""
+                    )
+                }
+            }
+            .flowOn(Dispatchers.IO)
+            .onEach {
+                _items.value = it
+            }
+            .launchIn(viewModelScope)
+    }
+
+    private val _items = MutableLiveData<List<HomeItemViewModel>>()
+    private val items: LiveData<List<HomeItemViewModel>> get() = _items
 }
 
 class HomeItemViewModel(
