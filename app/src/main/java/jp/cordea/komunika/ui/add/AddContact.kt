8dcfad5f5bf19cbbab11b78343b0a484955ba3bb
Yoshihiro Tanaka <contact@cordea.jp>
2021-09-18 13:49:53 +0900

    add text fields

--- app/src/main/java/jp/cordea/komunika/ui/add/AddContact.kt
+++ app/src/main/java/jp/cordea/komunika/ui/add/AddContact.kt
@@ -1,19 +1,75 @@
 package jp.cordea.komunika.ui.add
 
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun AddContact(viewModel: AddContactViewModel) {
     Scaffold(
-        topBar = { TopAppBar(title = { Text(text = "Add contact") }) }
+        topBar = { TopAppBar(title = { Text(text = "Add contact") }) },
+        floatingActionButton = {
+            FloatingActionButton(onClick = viewModel::onFabClicked) {
+                Icon(Icons.Default.Add, contentDescription = "Add contact")
+            }
+        }
     ) {
-        Content()
+        Content(viewModel)
     }
 }
 
 @Composable
-private fun Content() {
+private fun Content(viewModel: AddContactViewModel) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Row {
+            val firstName by viewModel.firstName.collectAsState()
+            OutlinedTextField(
+                value = firstName,
+                onValueChange = viewModel::onFirstNameChanged,
+                label = { Text("First name") },
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            val lastName by viewModel.lastName.collectAsState()
+            OutlinedTextField(
+                value = lastName,
+                onValueChange = viewModel::onLastNameChanged,
+                label = { Text("Last name") },
+                modifier = Modifier.weight(1f)
+            )
+        }
+        val phoneNumber by viewModel.phoneNumber.collectAsState()
+        OutlinedTextField(
+            value = phoneNumber,
+            onValueChange = viewModel::onPhoneNumberChanged,
+            label = { Text("Phone number") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)
+        )
+        val emailAddress by viewModel.emailAddress.collectAsState()
+        OutlinedTextField(
+            value = emailAddress,
+            onValueChange = viewModel::onEmailAddressChanged,
+            label = { Text("Email address") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)
+        )
+        val company by viewModel.company.collectAsState()
+        OutlinedTextField(
+            value = company,
+            onValueChange = viewModel::onCompanyChanged,
+            label = { Text("Company") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)
+        )
+    }
 }
--- app/src/main/java/jp/cordea/komunika/ui/add/AddContactViewModel.kt
+++ app/src/main/java/jp/cordea/komunika/ui/add/AddContactViewModel.kt
@@ -2,8 +2,48 @@ package jp.cordea.komunika.ui.add
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class AddContactViewModel @Inject constructor() : ViewModel() {
+    private val _firstName = MutableStateFlow("")
+    val firstName get() = _firstName.asStateFlow()
+
+    private val _lastName = MutableStateFlow("")
+    val lastName get() = _lastName.asStateFlow()
+
+    private val _phoneNumber = MutableStateFlow("")
+    val phoneNumber get() = _phoneNumber.asStateFlow()
+
+    private val _emailAddress = MutableStateFlow("")
+    val emailAddress get() = _emailAddress.asStateFlow()
+
+    private val _company = MutableStateFlow("")
+    val company get() = _company.asStateFlow()
+
+    fun onFirstNameChanged(firstName: String) {
+        _firstName.tryEmit(firstName)
+    }
+
+    fun onLastNameChanged(lastName: String) {
+        _lastName.tryEmit(lastName)
+    }
+
+    fun onPhoneNumberChanged(phoneNumber: String) {
+        _phoneNumber.tryEmit(phoneNumber)
+    }
+
+    fun onEmailAddressChanged(emailAddress: String) {
+        _emailAddress.tryEmit(emailAddress)
+    }
+
+    fun onCompanyChanged(company: String) {
+        _company.tryEmit(company)
+    }
+
+    fun onFabClicked() {
+
+    }
 }
